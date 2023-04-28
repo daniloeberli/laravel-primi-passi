c5755b42042b0791375aa89b5ca7dc09065e3932
daniloeberli <danilo.eberli@gmail.com>
2023-04-28 16:05:53 +0200

    feat: view data with Blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,7 @@
     <title>Home</title>
 </head>
 <body>
-    <h1>Hello World</h1>
+    <h1>{{$title}}</h1>
+    <h2>{{$subtitle}}</h2>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+
+    $data = [
+        'title' => 'Un bel titolo',
+        'subtitle' => 'sottotitolo interessante'
+    ];
+
+
+    return view('home', $data);
 });
